b885a4b3fe5645b45942eb5ace3d5cf461b58577
Libalee <joaovictor.alvino@gmail.com>
2024-06-03 13:22:04 -0300

    Servicices Implemented

--- com.biblioteca/src/main/java/com/biblioteca/services/BookServices.java
+++ com.biblioteca/src/main/java/com/biblioteca/services/BookServices.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.biblioteca.converter.MyModelMapper;
+import com.biblioteca.data.model.Book;
+import com.biblioteca.data.object.BookDO;
 import com.biblioteca.repository.BookRepository;
 
 @Service
@@ -13,5 +15,37 @@ public class BookServices {
 	BookRepository bookRepository;
 	
 	MyModelMapper mapper = new MyModelMapper();
+	
+	public BookDO create(BookDO bookDO) {
+		Book entity = mapper.parseBookDOToBook(bookDO, Book.class);
+		bookRepository.save(entity);
+		return bookDO;
+	}
+	
+	public BookDO findbyID(Long Id) {
+		var entity = bookRepository.findById(Id).orElseThrow();
+		BookDO bookDO = mapper.parseBookToBookDO(entity, BookDO.class);
+		return bookDO;
+	}
+	
+	public BookDO uptade(BookDO bookDO) {
+		Book book = bookRepository.getReferenceById(bookDO.getId());
+		
+		book.setAutor(bookDO.getAutor());
+		book.setAvaliable(bookDO.isAvaliable());
+		book.setBorrowingDate(bookDO.getBorrowingDate());
+		book.setName(bookDO.getName());;
+		book.setReserved(bookDO.isReserved());
+		book.setReturnDate(bookDO.getReturnDate());
+		book.setVolume(bookDO.getVolume());
+		
+		
+		return bookDO;
+	}
+	
+	public void Delete(Long id) {
+		Book entity = bookRepository.findById(id).orElseThrow();
+		bookRepository.delete(entity);
+	}
 	
 }
--- com.biblioteca/src/main/java/com/biblioteca/services/UserServices.java
+++ com.biblioteca/src/main/java/com/biblioteca/services/UserServices.java
@@ -4,15 +4,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.biblioteca.converter.MyModelMapper;
+import com.biblioteca.data.model.Book;
+import com.biblioteca.data.model.User;
+import com.biblioteca.data.object.UserDO;
 import com.biblioteca.repository.UserRepository;
 
 @Service
 public class UserServices {
 
 	@Autowired
-	UserRepository repository;
+	UserRepository userRepository;
 	
 	MyModelMapper mapper = new MyModelMapper();
 	
+	public UserDO create(UserDO userDO) {
+		User entity = mapper.parseUserDOToUser(userDO, User.class);
+		userRepository.save(entity);
+		return userDO;
+	}
+	
+	public UserDO findById(Long Id) {
+		var entity = userRepository.findById(Id).orElseThrow();
+		UserDO userDO = mapper.parseUserToUserDO(entity, UserDO.class);
+		return userDO;
+	}
+	
+	public UserDO update(UserDO userDO) {
+		User user = userRepository.findById(userDO.getId()).orElseThrow();
+		
+		user.setFineValue(userDO.getFineValue());
+		user.setItemsTaken(mapper.parseListBook(userDO.getItemsTaken(), Book.class));
+		user.setReservedItems(mapper.parseListBook(userDO.getReservedItems(), Book.class));
+		user.setLibraryID(userDO.getLibraryID());
+		user.setLibraryPassoword(userDO.getLibraryPassoword());
+		user.setName(userDO.getFullName());
+		
+		return userDO;
+	}
+	
+	public void delete(Long Id) {
+		User user = userRepository.findById(Id).orElseThrow();
+		userRepository.delete(user);
+	}
 	
 }
